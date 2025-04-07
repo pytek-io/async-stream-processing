b7ff9f1988b77223a122ed58de3d393668f9336e
fdv1 <francois@pytek.io>
2025-04-07 13:12:44 +0100

    exposed missing symbols

--- src/asp/__init__.py
+++ src/asp/__init__.py
@@ -1,8 +1,10 @@
-from .processor import run, now, EventStreamDefinition
+from .processor import call_later, run, sleep, now, EventStreamDefinition
 from . import testing
 
 __all__ = [
+    "call_later",
     "run",
+    "sleep",
     "now",
     "EventStreamDefinition",
     "testing",
