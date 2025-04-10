09392826d3ed612b602f3ea30cd6dce6ca0dba9e
fdv1 <francois@pytek.io>
2025-04-10 09:29:14 +0100

    clarified csp feedback example

--- examples/csp/02_intermediate/e1_feedback.py
+++ examples/csp/02_intermediate/e1_feedback.py
@@ -1,8 +1,10 @@
+from __future__ import annotations
 import asyncio
 from dataclasses import dataclass
 
 import asp
 from asp import BackgroundTaskDefinition
+from typing import Callable
 
 
 @dataclass
@@ -19,32 +21,40 @@ class ExecReport:
     status: str
 
 
-class Exchange:
-    def __init__(self):
-        self.quotes = {}
-        self.last_price = 100.0
+class MyAlgo:
+    def __init__(self, exchange: Exchange):
         self.last_id = 1
+        self.last_price = 100.0
+        self.exchange = exchange
 
-    def on_new_order(self, order: Order):
-        # await asyncio.sleep(0.7)
-        exec_report = ExecReport(order_id=order.order_id, status="ACK")
-        asp.call_later(0.7, self.my_algo, exec_report)
-
-    def my_algo(self, exec_report: ExecReport = None):
+    def on_exec_report(self, exec_report: ExecReport = None):
         order = Order(order_id=self.last_id, price=self.last_price, qty=200, side="BUY")
         self.last_id += 1
         self.last_price += 0.01
         print(f"Sending new order id:{order.order_id} price {order.price}")
-        asp.call_later(1, self.on_new_order, order)
+        asp.call_later(1, self.exchange.on_new_order, self.on_exec_report, order)
         if exec_report:
             print(
                 f"Received exec report for order id:{exec_report.order_id} status {exec_report.status}"
             )
 
 
+class Exchange:
+    def __init__(self):
+        self.quotes = {}
+        self.last_price = 100.0
+        self.last_id = 1
+
+    def on_new_order(self, exec_callback: Callable, order: Order):
+        # await asyncio.sleep(0.7)
+        exec_report = ExecReport(order_id=order.order_id, status="ACK")
+        asp.call_later(0.7, exec_callback, exec_report)
+
+
 def main():
     exchange = Exchange()
-    asyncio.run(asp.run([BackgroundTaskDefinition(callback=exchange.my_algo)]))
+    algo = MyAlgo(exchange)
+    asyncio.run(asp.run([BackgroundTaskDefinition(callback=algo.on_exec_report)]))
 
 
 if __name__ == "__main__":
